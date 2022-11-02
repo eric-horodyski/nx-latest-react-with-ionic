611f8dba130d47240a22335a811fc7c4ff2ce9e4
Eric Horodyski <horodyski@ionic.io>
2022-11-02 08:48:51 -0400

    Initial commit

---
+++ apps/nx-latest-react-with-ionic/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="nx-latest-react-with-ionic" />
+      <div />
+    </>
+  );
+}
+
+export default App;
